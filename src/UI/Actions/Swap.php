daa253166eaae7bb5bbe2acb2ea4d6ecec8b68d7
Fredxd <frederic@appolo.fr>
2025-06-18 14:00:26 +0200

    swap component

---
+++ src/UI/Actions/Swap.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppoloDev\UIToolboxBundle\UI\Actions;
+
+use AppoloDev\UIToolboxBundle\Attribute\UIComponentDoc;
+use AppoloDev\UIToolboxBundle\Attribute\UIComponentExample;
+use AppoloDev\UIToolboxBundle\Attribute\UIComponentProp;
+use AppoloDev\UIToolboxBundle\Resolver\ComponentOptionsResolverTrait;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('swap', template: '@UIToolbox/ui/actions/swap.html.twig')]
+#[UIComponentDoc(
+    title: 'Swap',
+    description: 'A customizable swap component based on DaisyUI. Supports style and more.',
+    tags: ['swap', 'daisyui', 'form', 'UI']
+)]
+#[UIComponentExample(
+    title: 'Swap text',
+    description: 'Text swaps on toggle',
+    templateName: '@UIToolboxDoc/examples/actions/swap/swap-text.html.twig',
+)]
+#[UIComponentExample(
+    title: 'Swap volume icons',
+    description: 'Volume on/off icons swap',
+    templateName: '@UIToolboxDoc/examples/actions/swap/swap-volume-icons.html.twig',
+)]
+#[UIComponentExample(
+    title: 'Swap icons with rotate effect',
+    description: 'Icons swap with rotate transition',
+    templateName: '@UIToolboxDoc/examples/actions/swap/swap-rotate.html.twig',
+)]
+#[UIComponentExample(
+    title: 'Hamburger button',
+    description: 'Hamburger icon toggle for menu',
+    templateName: '@UIToolboxDoc/examples/actions/swap/swap-hamburger.html.twig',
+)]
+#[UIComponentExample(
+    title: 'Swap icons with flip effect',
+    description: 'Icons flip on toggle',
+    templateName: '@UIToolboxDoc/examples/actions/swap/swap-flip.html.twig',
+)]
+class Swap
+{
+    use ComponentOptionsResolverTrait;
+
+    #[UIComponentProp(label: 'Style', type: 'string|null', description: 'Swap style', default: null, acceptedValues: [null, 'rotate', 'flip'])]
+    public ?string $style = null;
+
+    #[UIComponentProp(label: 'Classes CSS', type: 'string|null', description: 'Additional CSS classes', default: null)]
+    public ?string $class = null;
+
+    public function getClasses(): string
+    {
+        $classes = ['swap'];
+
+        if ($this->style !== null) {
+            $classes[] = "swap-{$this->style}";
+        }
+
+        if ($this->class) {
+            $classes[] = $this->class;
+        }
+
+        return \implode(' ', $classes);
+    }
+}
